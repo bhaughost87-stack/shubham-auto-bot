7354f1c6be5446437d48306075415a1553105212
bhaughost87-stack <bhaughost87@gmail.com>
2026-04-06 01:30:13 +0530

    Update APK ID and message for bot response

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ API_HASH = "0230fa05102dc2819b46fa00abbe7fd9"
 BOT_TOKEN = "8664894205:AAGvnqPloabIfIe0sV_wZG7H1roghJq5ONg"
 
 # TERA APK ID
-APK_ID = "BQACAgUAAxkBAAMDac6_MuJOBGoKXbzT_oK606S77mgAAnccAAKamnhWGj7GkWbfLDUeBA"
+APK_ID = "BQACAgUAAxkBAAMHadEVOlUCXj8zQCOcgsaJDyX52b0AAq0aAALjflFWa0EWTZiLPYgeBA"
 
 app = Client("auto_sender", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
@@ -18,7 +18,16 @@ async def send_apk(client, request: ChatJoinRequest):
         await client.send_document(
             chat_id=request.from_user.id, 
             document=APK_ID, 
-            caption="🚀 **SHUBHAM X MOD**\n\nLoot start karo! APK install karke login karo. 🔥"
+            caption="🚀 **🎉 ( ONLY FOR PREMIUM USERS  ) 💥( 100% LOSS RECOVER GUARANTEE ) 😵
+Hack future add ☠️
+Wingo-Number Hack
+Trx1Min-Number Hack
+
+     💸Click and install💸
+
+How To Use and download Hack 👇
+https://t.me/JaiclubNumberHack/5
+https://t.me/JaiclubNumberHack/5. 🔥"
         )
         print(f"✅ APK Sent to: {request.from_user.id}")
     except Exception as e:
@@ -26,4 +35,3 @@ async def send_apk(client, request: ChatJoinRequest):
 
 print("🚀 Bot LIVE hai! Jo bhi channel join request bhejega, use APK mil jayega.")
 app.run()
-
